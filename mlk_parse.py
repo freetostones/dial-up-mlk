69d8d40983711b09de40292e84e698089cdb5bae
yawkity <sehmon3@gmail.com>
2016-06-29 23:53:29 -0400

    Refactored parse funciton

--- mlk_parse.py
+++ mlk_parse.py
@@ -1,12 +1,24 @@
+"""
+Parser to find whether a Shapefile record contains the following substrings:
+    ["martin luther", "mlk", "m.l.k"]
+"""
+
 import shapefile
-print('Opening file')
-sf = shapefile.Reader("shapefiles/roads")
+import sys
 
-mlk_streets = []
+def main(unused_args):
+  sf = shapefile.Reader("shapefiles/roads")
+  mlk_streets = find_mlk_streets(sf)
+  print mlk_streets
 
-for record in sf.records():
-    name = record[1].lower()
+
+def find_mlk_streets(shapefile_input):
+  streets = []
+  for record in shapefile_input.records():
+    name = record[1].lower()  # Ignore capitalization errors
     if ('martin luther' in name) or ('mlk' in name) or ('m.l.k' in name):
-        mlk_streets.append(record)
+      streets.append(record)
+  return streets
 
-print(mlk_streets)
+if __name__ == "__main__":
+  main(sys.argv)
